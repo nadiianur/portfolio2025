bfa4eaa34ebdab05b8c84909b8a2787d342c0b03
Nadia Nur Saida <nadianrs88@gmail.com>
2025-07-21 17:35:33 +0700

    Update route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import OrganizationalExperience from "./components/OrganisationalExperiences";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/organizational-experience" element={<OrganizationalExperience />} />
+        <Route path="/profile2025" element={<Home />} />
+        <Route path="/profile2025/organizational-experience" element={<OrganizationalExperience />} />
       </Routes>
     </Router>
   );
