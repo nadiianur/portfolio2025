32b0a7c90adc8d80213e445da2db8d3bceff1faf
Nadia Nur Saida <nadianrs88@gmail.com>
2025-07-21 21:01:51 +0700

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import OrganizationalExperience from "./components/OrganisationalExperiences.js";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/profile2025">
       <Routes>
-        <Route path="/profile2025" element={<Home />} />
-        <Route path="/profile2025/organizational-experience/" element={<OrganizationalExperience />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/organizational-experience" element={<OrganizationalExperience />} />
       </Routes>
     </Router>
   );
